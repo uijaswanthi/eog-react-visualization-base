bc5c1e4d603564d663cd445321e6a711ddae0be0
Jaswanthi Priya <jaswanthipriya65@gmail.com>
2020-06-24 20:39:30 +0530

    Create reducer.ts

---
+++ src/Features/Metrics/reducer.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from 'redux-starter-kit';
+
+export type ApiErrorAction = {
+  error: string;
+}
+
+interface IMetricsState {
+  metrics: string[];
+  selectedMetrics: string[];
+  measurementData: any;
+  graphData: any;
+}
+
+const initialState: IMetricsState = {
+  metrics: [],
+  graphData: {},
+  measurementData: {},
+  selectedMetrics: []
+}
+
+const slice = createSlice({
+  name: 'metric',
+  initialState,
+  reducers: {
+    setMetrics: (state, action) => {
+      state.metrics = action.payload;
+    },
+    updateSelected: (state, action) => {
+      state.selectedMetrics = action.payload;
+      const measurementData = state.measurementData;
+      action.payload.forEach((metric: string) => {
+        if(!measurementData[metric]) {
+          measurementData[metric] = {};
+        }
+      });
+      state.measurementData = measurementData;
+    },
+    metricsApiErrorAction: (state, action: PayloadAction<ApiErrorAction>) => state,
+  }
+})
+
+export const reducer = slice.reducer;
+export const actions = slice.actions;
+
