2257c9ba842f0613ced0f5f00180dfbb80f723a8
Jaswanthi Priya <jaswanthipriya65@gmail.com>
2020-06-24 20:49:10 +0530

    addred metrics reducer to root reducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,5 +1,7 @@
+import { reducer as metricReducer } from '../Features/Metrics/reducer';
 import { reducer as weatherReducer } from '../Features/Weather/reducer';
 
 export default {
+  metric: metricReducer,
   weather: weatherReducer,
 };
