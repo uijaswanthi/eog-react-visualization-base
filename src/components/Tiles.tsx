0e406e9c818e6c36486487d2e5c40920f97fcb44
Jaswanthi Priya <jaswanthipriya65@gmail.com>
2020-06-24 20:54:08 +0530

    Create Tiles.tsx

---
+++ src/components/Tiles.tsx
@@ -0,0 +1,50 @@
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+
+export default () => {
+  return <Tiles />;
+};
+
+const getMeasurementData = (state: any) => {
+  const { measurementData } = state.metric;
+  return measurementData;
+};
+
+const Tiles = () => {
+  const measurementData = useSelector(getMeasurementData);
+  const list = useMemo(() => {
+    const mList = [];
+    for(const metric in measurementData) {
+      if(measurementData[metric] && measurementData[metric].metric) {
+        mList.push(measurementData[metric]);
+      }
+    }
+    return mList;
+  }, [measurementData]);
+  
+  return (
+    <div>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+        {list
+          ? list.map(a => {
+            return (
+              <Grid key={a.metric} item sm={2} xs={1}>
+                <Card>
+                  <CardContent>
+                    <Typography component="h5" variant="h5">{a.value} {a.unit}</Typography>
+                    <Typography variant="subtitle1" color="textSecondary">{a.metric}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })
+          : null}
+      </Grid>
+    </div>
+  );
+};
+
